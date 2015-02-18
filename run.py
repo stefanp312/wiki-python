2750b42915e17874b0bf5642214e9c2350f44ea4
Stefan Pleava <stefan@osellus-mobile.com>
2015-02-18 01:10:07 -0500

    attempt 6

--- run.py
+++ run.py
@@ -1,8 +1,29 @@
-import os
-from flask import Flask
+from flask import Flask, request, redirect
+import twilio.twiml
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+# Try adding your own number to this list!
+callers = {
+    "+14158675309": "Curious George",
+    "+14158675310": "Boots",
+    "+14168343783": "Virgil",
+}
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond and greet the caller by name."""
+
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
+
+    resp = twilio.twiml.Response()
+    resp.message(message)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
