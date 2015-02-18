81d939cda694b2a98074f625c1e25e939da9782e
Stefan Pleava <stefan@osellus-mobile.com>
2015-02-17 22:29:09 -0500

    new demo

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Greeting
@@ -6,9 +7,8 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES',2))
+    return HttpResponse('Hello!'*times)
 
 
 def db(request):
