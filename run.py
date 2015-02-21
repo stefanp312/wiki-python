3e52ac3858d95129a01a7da067dcf941857a61b1
Stefan Pleava <stefan@osellus-mobile.com>
2015-02-20 23:31:41 -0500

    see more functionality with cookies

--- run.py
+++ run.py
@@ -13,31 +13,52 @@ def main_reply():
     
     from_number = request.values.get('From', None)
     recieved_message = request.values.get('Body')
-    counter = session.get('counter', 0)
- 
-    # increment the counter
-    counter += 1
- 
-    # Save the new counter value in the session
-    session['counter'] = counter
 
-    reply = searchWikipedia(query=recieved_message)
+    #create cmds variable from cookies
+    cmds = session.get('cmds', [""])
+    searchs = session.get('searchs', [["",0]])
+
+    if "image" in recieved_message:
+        #send the wikipedia page iamge
+        print "not working"
+    else if "more" in recieved_message:
+        # query wikipedia with the recieved message and get next sentence
+        cmds.append("more")
+        lastSearch = searchs[len(searchs)-1]
+        print lastSearch
+        lastCharacterSeen = lastSearch[1]
+        print lastCharacterSeen
+        reply = searchWikipedia(query=recieved_message,4)
+        nextStringIndex =  reply.index(". ", beg=lastCharacterSeen)
+        reply = reply[lastCharacterSeen:nextStringIndex]
+    else
+        # query wikipedia with the recieved message
+        reply = searchWikipedia(query=recieved_message)
+        searchs.append([recieved_message,len(reply)])
+
+    #trim the length of the reply to one text
     if len(reply)>160:
         reply=reply[0:159]
+
+    #get the response scheme from twilio and add reply as message body
     resp = twilio.twiml.Response()
     resp.message(reply)
 
     print reply
-    print counter
+
+    # Save the new cmds/searchs list in the session
+    session['cmds'] = cmds
+    session['searchs'] = searchs
+
     return str(resp)
 
-def searchWikipedia(query):
+def searchWikipedia(query, sentences = 1):
     summary = ""
     try:
-        summary = wikipedia.summary(query, sentences = 1)
+        summary = wikipedia.summary(query, sentences = sentences)
     except wikipedia.exceptions.DisambiguationError as e:
         #return "That can refer to multiple things. Which did you mean? " + ", ".join(e.options)
-        summary = wikipedia.summary(e.options[1], sentences = 1)
+        summary = wikipedia.summary(e.options[1], sentences = sentences)
     return summary
 
 if __name__ == "__main__":
