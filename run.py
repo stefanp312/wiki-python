7ab849056c179e67e735772b1595d44f273f9aeb
Stefan Pleava <stefan@osellus-mobile.com>
2015-02-20 22:09:48 -0500

    yes please cookies

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 import twilio.twiml
 import wikipedia
 
+SECRET_KEY = 'donuts'
 app = Flask(__name__)
 
 # Try adding your own number to this list!
@@ -11,6 +12,7 @@ def main_reply():
     
     from_number = request.values.get('From', None)
     recieved_message = request.values.get('Body')
+    counter = session.get('counter',0)
 
     reply = searchWikipedia(query=recieved_message)
     if len(reply)>160:
@@ -18,7 +20,10 @@ def main_reply():
     resp = twilio.twiml.Response()
     resp.message(reply)
 
-    print "Test this shit"
+    counter+=1
+    session['counter']=counter
+    print reply
+    print counter
     return str(resp)
 
 def searchWikipedia(query):
